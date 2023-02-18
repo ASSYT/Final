4dad50dec1ac49dfc9da42b7f699221c657970f0
Asy_student <asb_mogilev@mail.ru>
2023-02-18 19:34:40 +0300

    Метод заполнения массива по условию

--- Final_test_work_1/Program.cs
+++ Final_test_work_1/Program.cs
@@ -7,3 +7,19 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] FillingtArray(string[] arrayString) // заполнение нового массива по условию
+{
+    int z = 0;
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        if (arrayString[i].Length <= 3) z++;
+    }
+    string[] resultArray = new string[z];
+    z=0;
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        if (arrayString[i].Length <= 3) resultArray[z++] = arrayString[i];
+    }
+return resultArray;
+}
